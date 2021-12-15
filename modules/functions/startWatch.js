33723394c20e0fd8cca515e4600b7890d99ea320
Nirav Shah <nirav@ventureoneit.com>
2021-12-15 09:34:56 +0530

    Version 1 (#2)
    * Initial setup and partially working
    
    - Pub Sub setup completed
    - Notifications working to some extent
    - Sample credentials and config files created
    - DevContainer env created and working
    
    Todo
    - Need to refine the filter for notifications.
    - Separate functions into their own files and dynamically load them based on ENV variables. (reduce cold start time)
    - Work in progress on JEST test files
    
    * Preliminary project setup completed
    
    - Added rollup for bundling and publishing the project
    - Ensured it outputs a .zip file in the publish folder for upload to GCP
    
    * Implemented tests and separated functionality into modules
    
    - Separated Storage and Gmail API into their own modules.
    - Created basic Cloud Function modules which can be imported as per environment variables in the future.
    - Implemented test files for Storage and Gmail API modules.
    - Updated jest extension settings as well as jest timeout  settings (within package.json file).
    
    * Implemented and successfully tested functions
    
    - Updated the readme.md file with proper details
    - Implemented the processMessage functionality in line with the Gmail history flow logic
    - Corrected the bug for getting two notifications for each email.
    - Updated the code to account for labelAdded and messageAdded variables.
    - Updated appconfig.json file structure to rename `slack` with `external` for future proofing the code.
    
    * Optimized the code for better execution + bug fix
    
    - Added performance timer to measure function execution
    - Fixed a bug where notification is sent when an unread item is moved to trash (since TRASH label  is added), added a check so only send notification if labels being monitored are added.
    
    * Basic code documentation implemented and updated devcontainer files
    
    - Added JSDoct comments for all the major functions and helper functions
    - Added DocThis vscode extension to help with JSDoc comment block generation via Ctrl + Alt + D (twice)
    - Added the commend to install "zip" package to the Dockerfile
    
    * Fixed bug in startWatch function
    
    - Fixed a bug in startWatch which gives error "Only one user push notification client allowed per developer"
    - Updated version to 0.0.16

--- modules/functions/startWatch.js
+++ modules/functions/startWatch.js
@@ -11,8 +11,11 @@ const gmail = require("../gmail");
 exports.startWatch = async (req, res) => {
     try {
       const authGmail = await gmail.getAuthenticatedGmail();
-      const resp = await authGmail.users.watch({
-        'userId': 'me',
+      var resp = await authGmail.users.stop({
+        userId: 'me',
+      });
+      resp = await authGmail.users.watch({
+        userId: 'me',
         topicName: appConfig.gcp.pubsub.topic,
         labelIds: appConfig.gmail.labelsIds,
         labelFilterAction: appConfig.gmail.filterAction
@@ -24,4 +27,3 @@ exports.startWatch = async (req, res) => {
       throw new Error("Error occured while starting gmail watch: " + ex);
     }
   };
-  
